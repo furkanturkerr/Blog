909f34c92c3c08a53f743d2978a6ae402a51fec5
furkanturker <furkanturker.dev@gmail.com>
2025-12-19 23:29:49 +0300

    Mail gönderme işlemi eklendi ve admin panelinde düzenlemeler yapıldı

---
+++ DataAccess/Migrations/20251219184953_update_contact.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data_Access_Layer.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_contact : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Contacts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Message",
+                table: "Contacts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Contacts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "Message",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Contacts");
+        }
+    }
+}
---
+++ DataAccess/Migrations/20251219191052_update_contac_deletemap.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data_Access_Layer.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_contac_deletemap : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactMap",
+                table: "Contacts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactMap",
+                table: "Contacts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
